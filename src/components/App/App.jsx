ec8b0eb5c87b2f8f699b92f723ef22545b901bb9
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2022-10-21 16:32:17 +0300

    finish

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -64,6 +64,21 @@ export class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     return (
       <>
